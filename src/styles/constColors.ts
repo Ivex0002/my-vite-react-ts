81195cd6eeb053058fe9d6d70045aeaabfd0a917
ivex0002 <garam199803@gmail.com>
2025-08-30 19:36:14 +0900

    주석 수정

--- src/styles/constColors.ts
+++ src/styles/constColors.ts
@@ -17,15 +17,4 @@ export const colors = { dark, light } as const;
 export type ColorTheme = keyof typeof colors; // 'dark' | 'light'
 export const themeKeys = Object.keys(colors) as ColorTheme[]; // ['dark', 'light']
 
-// function MyComponent() {
-//   const darkMode = useStore(state => state.darkMode);
-//   const mode = darkMode ? 'dark' : 'light';
-//   // 3가지 이상의 색상모드의 경우 type 또는 상수 지정해서 사용
-//   const { text, background } = colors[mode];
 
-//   const Box = styled.div`
-//     color: ${text};
-//     background-color: ${background};
-//   `;
-//   return <Box>Hello</Box>;
-// }
